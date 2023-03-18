54b11c06f2498774d01bcc1908f3f5dc3b8d22fc
Danchiiik <dcabatar@gmail.com>
2023-03-18 21:02:40 +0600

    profile 1.0

--- applications/profiles/views.py
+++ applications/profiles/views.py
@@ -1,14 +1,11 @@
-
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import mixins, GenericViewSet
 
 from applications.profiles.models import UserProfile
+from applications.profiles.permissions import IsProfileOwner
 from applications.profiles.serializers import ProfileSerializer
 
-class ProfileViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet):
+class ProfileViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet):
     serializer_class = ProfileSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsProfileOwner]
     queryset = UserProfile.objects.all()
     
-    def get_queryset(self):
-        return UserProfile.objects.filter(user=self.request.user)
